f09f5c3ef6c1a041c654eeb7094a36583259dc66
NIKOLAY VASILEV <vasilievkolay@yandex.ru>
2023-06-29 22:24:14 +0300

    пофиксил баги по верстке

--- src/Components/backgroundWeather/backgroundWeather.jsx
+++ src/Components/backgroundWeather/backgroundWeather.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import './backgroundWeather.css'
 
-export const backgroundWeather = ({ cards }) => {
+export const BackgroundWeather = ({ cards }) => {
     return (
-    <div className="container">
         <div className='clouds'>
             <div className='clouds_1'></div>
             <div className='clouds_2'></div>
             <div className='clouds_3'></div>
         </div>
-    </div>
     )
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -5,28 +5,20 @@ import './Components/backgroundWeather/backgroundWeather.css'
 import { Header } from './Components/header/header.jsx'
 import { Search } from './Components/search/search.jsx'
 import { CardsByHours } from './Components/cardsByHours/cardsByHours.jsx'
-import { mainInformationCities } from './Components/mainInformationCities/mainInformationCities.jsx'
+import { MainInformationCities } from './Components/mainInformationCities/mainInformationCities.jsx'
+import { BackgroundWeather } from './Components/backgroundWeather/backgroundWeather.jsx' 
+
 
 const Main = ({ cards }) => {
   return (
     <div className="container">
-      <div className='clouds'>
-          <div className='clouds_1'></div>
-          <div className='clouds_2'></div>
-          <div className='clouds_3'></div>
-            <div className='card_text'>
-              <span className='city'>Москва</span>
-              <span className='сelsius'>22&deg;</span>
-              <span className='weather_description'>Временами облачно</span>
-              <span className='max_min_temperature'>Макс.: 23&deg;, мин.: 12&deg;</span>
-            </div>
-      </div>
+          <BackgroundWeather />
           <Header />
           <Search />
-          <mainInformationCities />
+          <MainInformationCities />
           <CardsByHours />
     </div>
   )
 }
 
-export default Main
+export default Mainтз
